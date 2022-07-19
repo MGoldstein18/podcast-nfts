148b96515045cd3dbd2b26650b445fd15952d205
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2022-07-19 11:29:14 +0200

    refactor(app.tsx): add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import Script from 'next/script';
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Polygon;
@@ -9,7 +10,22 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <ChakraProvider>
-      <Component {...pageProps} />
+        <Script
+          strategy='lazyOnload'
+          src={`https://www.googletagmanager.com/gtag/js?id=G-NL8RR637N2`}
+        />
+
+        <Script id='1' strategy='lazyOnload'>
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-NL8RR637N2', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </Script>
+        <Component {...pageProps} />
       </ChakraProvider>
     </ThirdwebProvider>
   );
